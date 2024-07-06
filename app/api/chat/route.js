3e06ec0bcc20056c889fdbfc35723588fc699c37
Sadanand <sadachaudhari@gmail.com>
2024-07-06 17:00:11 +0530

    gemini-1.5-flash implemented

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,15 +1,26 @@
-import { google } from '@ai-sdk/google';
-import { streamText } from 'ai';
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY);
+
+export async function POST(req, res) {
 
 
-export async function POST(req) {
+const { messages } = await req.json();
 
-    const { messages } = await req.json();
 
-    const result = await streamText({
-      model: google('models/gemini-1.5-flash-latest'),
-      messages,
-    });
-  
-    return result.toAIStreamResponse();
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const result = await model.generateContentStream(messages[0].content);
+
+  //   const response = await result.response;
+  //   const text = response.text();
+  let text = "";
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    // console.log(chunkText);
+    text += chunkText;
   }
+
+  return NextResponse.json({ text });
+}
